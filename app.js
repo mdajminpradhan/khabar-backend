853e49dd269e2471967b31436b6f64a03022a7fc
mdajminpradhan <md.ajminpradhan@outlook.com>
2021-11-03 13:19:11 +0600

    gitignore modified

--- app.js
+++ app.js
@@ -1,51 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 // middlewares
-
 app.use(express.json());
 app.use(cors());
 
 // connecting mongodb
-
 mongoose
-	.connect(process.env.DATABASE, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true
-	})
-	.then(() => {
-		console.log('Database connected successfully');
-	})
-	.catch((error) => {
-		console.log('Database connection failed', error);
-	});
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log("Database connection failed", error);
+  });
 
 // bringing all routes
-
-const user = require('./routes/user');
-const product = require('./routes/product')
-const category = require('./routes/category');
-const postcategory = require('./routes/postcategory');
-const post = require('./routes/post');
+const user = require("./routes/user");
+const product = require("./routes/product");
+const category = require("./routes/category");
+const postcategory = require("./routes/postcategory");
+const post = require("./routes/post");
 
 // making api
-
-app.use('/api', user);
-app.use('/api', product);
-app.use('/api', category);
-app.use('/api', postcategory);
-app.use('/api', post);
+app.use("/api", user);
+app.use("/api", product);
+app.use("/api", category);
+app.use("/api", postcategory);
+app.use("/api", post);
 
 // assigning port
-
 const port = process.env.PORT || 5000;
 
 // creating server
 app.listen(port, () => {
-	console.log('Server started successfully...');
+  console.log("Server started successfully...");
 });
